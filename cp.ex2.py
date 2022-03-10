b168a62f693deac3431299cb1719e4a63a65b16f
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-03-10 12:22:02 +0900

    Add files via upload

---
+++ cp.ex2.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Turtle()
+
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+turtle.mainloop()
+turtle.bye()
