d128da16601212d2d90bf87af4a35b4037de56c0
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-06-10 23:42:37 +0900

    Add files via upload

--- 20192618김재민_3.py
+++ 20192618김재민_3.py
@@ -1,48 +1,18 @@
+import random
 import turtle
+t = turtle.Turtle()
 
-t= turtle.Turtle()
-t.shape("turtle")
-side = int(input("한 변의 길이는?"))
-angle = 90
+def draw_shape(c, length, sides, x, y):
+    t.up(); t.goto(x, y); t.down(); t.fillcolor(c); angle = 360.0/sides; t.begin_fill()
+    for dist in range(sides):
+        t.forward(length); t.left(angle)
+    t.end_fill()
 
-t.forward(side)
-t.right(angle)
+for i in range(10):
+    color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green'])
+    side_length = random.randint(10, 100); sides = random.randint(3, 10)
+    x = random.randint(-200, 200); y = random.randint(-200, 200)
+    draw_shape(color, side_length, sides, x, y) 
 
-t.forward(side)
-t.right(angle)
 
-t.forward(side)
-t.right(angle)
-
-t.forward(side)
-t.right(angle)
-
-t.left(angle)
-t.left(angle)
-
-t.forward(side)
-t.left(angle)
-t.forward(side)
-t.left(angle)
-t.forward(side)
-t.left(angle)
-t.forward(side)
-
-t.forward(side)
-t.left(angle)
-t.forward(side)
-t.left(angle)
-t.forward(side)
-t.left(angle)
-t.forward(side)
-
-t.forward(side)
-t.left(angle)
-t.forward(side)
-t.left(angle)
-t.forward(side)
-t.left(angle)
-t.forward(side)
-
-turtle.mainloop()
-turtle.bye()
+turtle.done()
